3e6a9ecafa469c281ecd6415cafe8036a2a7ec9a
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-20 17:52:32 +0800

    JavaWebLearn

--- src/web/servlet/loginServlet.java
+++ src/web/servlet/loginServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet("/loginServlet")
 public class loginServlet extends HttpServlet {
     @Override
--- src/web/servlet/successServlet.java
+++ src/web/servlet/successServlet.java
@@ -1,11 +1,12 @@
 package web.servlet;
 
+
 import domain.User;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
